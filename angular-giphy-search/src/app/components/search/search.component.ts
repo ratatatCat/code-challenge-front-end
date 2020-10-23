19b1fa81c80a876d3a98d4b3936b042569bf8d35
ratatatcat <iretoncat@gmail.com>
2020-10-22 22:52:41 -0500

    Add paging

--- angular-giphy-search/src/app/components/search/search.component.ts
+++ angular-giphy-search/src/app/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { GiphyService } from '../../services/giphy.service';
 
 import { FormControl, FormGroup } from '@angular/forms';
@@ -13,16 +13,38 @@ export class SearchComponent implements OnInit {
     searchResult: new FormControl()
 }); 
 
-  giphyImages: any;
+  giphyImages: any[];
+  searchTerm: string;
+  searchPos: number = 0;
 
   constructor(private giphyService: GiphyService) { }
 
   ngOnInit() { }
 
   performSearch(searchTerm: any): void {
-    this.giphyService.getImages(searchTerm.searchResult).subscribe((res: any) => {
+    this.searchTerm = searchTerm.searchResult;
+
+    this.giphyService.getImages(this.searchTerm, this.searchPos).subscribe((res: any) => {
       console.log(res);
+      this.searchPos =+ res.data.length;
       this.giphyImages = res.data;
     });;
+  }
+
+  @HostListener("window:scroll", ["$event"])
+  onWindowScroll() {
+    let pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
+    let max = document.documentElement.scrollHeight;
+
+    if(pos == max )   {
+      if(this.giphyImages.length > 0) {
+        this.giphyService.getImages(this.searchTerm, this.searchPos).subscribe((res: any) => {
+          console.log(res);
+          res.data.forEach(image => {
+            this.giphyImages.push(image);
+          });;
+        });;
+      }
+    }
   }
 }
--- angular-giphy-search/src/app/services/giphy.service.ts
+++ angular-giphy-search/src/app/services/giphy.service.ts
@@ -9,12 +9,12 @@ export class GiphyService {
 
   apiUrl: string = 'https://api.giphy.com/v1/gifs/search';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
-  params = new HttpParams().set('api_key','4s8JFJyLgoLSgBXiEczjymJ6sGNNYSdW').set('rating','pg');
+  params = new HttpParams().set('api_key','4s8JFJyLgoLSgBXiEczjymJ6sGNNYSdW').set('rating','pg').set('limit','20');
 
   constructor(private http: HttpClient) { }
 
-  getImages(searchTerm: string) {
-    let params = this.params.set('q', searchTerm);
+  getImages(searchTerm: string, searchPos: number) {
+    let params = this.params.set('q', searchTerm).set('offset', searchPos.toString());
 
     return this.http.get(`${this.apiUrl}`, {params: params});
   }
